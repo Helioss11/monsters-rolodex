3179f8f4a4b97c8d47f3ca2edf939c1fb497e549
Helios Figueroa <hfigueroa@abargon.com>
2019-07-07 21:26:25 -0500

    Updates

---
+++ precache-manifest.2983b6ba5f4c8951bcedfd79011d5b27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad0dd5fe694b6f8146783ab07b3ddff",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1d1f32c71637454c4595",
+    "url": "/monsters-rolodex/static/css/main.0f50af3d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1d1f32c71637454c4595",
+    "url": "/monsters-rolodex/static/js/main.0d94816a.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
